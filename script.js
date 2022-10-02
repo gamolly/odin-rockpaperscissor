bfc8f4ecb6feb92b982d4118484ec57b2beb4f44
Gamolly <gamollygamolly@gmail.com>
2022-10-02 14:27:14 -0400

    Add pseudocode

---
+++ script.js
@@ -0,0 +1,19 @@
+/* 
+> create a list of 3 choices rock, paper, scissor (all lowercase) store in validChoice
+> get user choice and store in userChoice 
+    >make case-insensitive by transforming input to lowercase
+    >only accept valid choices that matches one of validChoice's
+        >keep asking until valid choice
+> Create a list of valid choices get random choice and store in randomChoice
+
+>Play game
+    > initial scores user (0) - computer (0)
+    >compare against randomized "Rock", "Paper", "Scissor" stored in randChoice
+    : if (userChoice == randomChoice) print "Draw! Play again!" >> restart game
+    : otherwise if (userChoice = Rock, randomChoice = paper OR user = paper, randomChoice = scissors
+        OR userChoice = scissor, randomChoice = Rock) print ("Computer Wins! Computer reigns supreme wow!")
+    : otherwise print ("User wins! User is very skilled wow!")
+    > increase winner's score by 1 
+
+> if userScore or randScore = 3, game ends 
+*/
